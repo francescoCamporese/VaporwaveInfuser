2fc553974ccce0c1acf283eefcb59f07b1ddc8e9
Francesco Camporese <francesco.camporese99@gmail.com>
2021-06-30 18:09:59 +0200

    wallpaper buttons scrollbar and new icons, title fix

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,9 +1,11 @@
 function selectBg(id) {
-    var vid = document.getElementById("bgVid");
     var src = document.getElementById("bgSrc");
-    src.src = "static/bg/" + id + ".mp4";
-    vid.load();
-    vid.play();
+    if (src.getAttribute("src").localeCompare("static/bg/" + id + ".mp4")) {    
+        var vid = document.getElementById("bgVid");
+        src.src = "static/bg/" + id + ".mp4";
+        vid.load();
+        vid.play();
+    }
 }
 
 /*window.onload = function() {
@@ -13,7 +15,7 @@ function selectBg(id) {
     fs.readdirSync("static/bg").forEach(name =>
     {
         var label = document.createElement("label");
-        label.setAttribute("class", "btn btn-primary");
+        label.setAttribute("class", "btn bg-transparent btn-lg text-white");
         label.setAttribute("id", "bglbl" + name);
         label.setAttribute("onclick", "selectBg(" + name + ");");
         
@@ -22,14 +24,13 @@ function selectBg(id) {
         input.setAttribute("name", "bg");
         input.setAttribute("id", "bg" + "name");
         input.setAttribute("autocomplete", "off");
-        input.innerHTML = name;
+        input.innerHTML = "<img src=\"static/img/98pc24dp.png\"><br>" + id;
         
         label.appendChild(input);
         div.appendChild(label);
     });
     
-    document.getElementById("bglbl1").ìsetAttribute("class", "btn btn-primary active");
+    document.getElementById("bglbl1").setAttribute("class", "btn bg-transparent btn-lg text-white active");
     document.getElementById("bg1").checked = true;//testare
     selectBg(1);
-}
-*/
+}*/
