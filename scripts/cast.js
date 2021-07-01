7e590afcd0759fc77eeafcc9c12e447175ebc67b
Francesco Camporese <francesco.camporese99@gmail.com>
2021-07-01 18:57:51 +0200

    new cast implementation

--- scripts/cast.js
+++ scripts/cast.js
@@ -1,4 +1,4 @@
-var initializeCastApi = function() {
+/*var initializeCastApi = function() {
   console.log('initializeCastApi');
 
   var sessionRequest = new chrome.cast.SessionRequest(
@@ -44,8 +44,43 @@ function onSessionRequestSuccess(session) {
 
 function onMediaLoadSuccess(e) {
   console.log('onMediaLoadSuccess', e);
+  $("#castBtn").html("<img src=\"static/img/baseline_cast_connected_white_24dp.png\">");
 }
 
 function onCastBtnClick() {
   chrome.cast.requestSession(onSessionRequestSuccess, onError);
-}
+}*/
+
+
+
+
+
+//TEST
+window.__onGCastApiAvailable = function(isAvailable){
+  if(! isAvailable) {
+    return false;
+  }
+
+  var castContext = cast.framework.CastContext.getInstance();
+  
+  castContext.setOptions({
+    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
+    receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
+  });
+
+  var stateChanged = cast.framework.CastContextEventType.CAST_STATE_CHANGED;
+  castContext.addEventListener(stateChanged, function(event){
+    var castSession = castContext.getCurrentSession();
+    var media = new chrome.cast.media.MediaInfo("https://www.example.com/my-song.mp3", "audio/mp3");
+    var request = new chrome.cast.media.LoadRequest(media);
+
+    castSession && castSession
+      .loadMedia(request)
+      .then(function() {
+        console.log("Success");
+      })
+      .catch(function(error) {
+        console.log("Error: " + error);
+      });
+  });
+};
