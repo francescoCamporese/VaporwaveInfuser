59ef4a4bc9a3c82cb4ee62aa6f18b4c8c4bf1308
Francesco Camporese <francesco.camporese99@gmail.com>
2021-10-08 18:16:47 +0200

    PWA implementation

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
     	<div class="container-fluid">
         	<?php include "home.php";?>
         </div>
+        <script async>
+            window.onload = () => {
+                'use strict'; 
+                if ('serviceWorker' in navigator) {
+                    navigator.serviceWorker.register('./service-worker.js');
+                }
+            }
+        </script>
     </body>
 </html>
---
+++ service-worker.js
@@ -0,0 +1,18 @@
+var cacheName = 'pwa-vaporwaveinfuser';
+var filesToCache = [
+    '/'
+];
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+    }));
+});
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+    }));
+});
+
+ 
